e6de9b7bfd3e8feba2625321a9a91b84b0190095
am22ar <amarpatil12222@gmail.com>
2022-10-03 18:19:02 +0530

    Added: Welcome to Gambling Simulator Program

---
+++ src/com/java/gamblingsimulator/Gambling_Simulator.java
@@ -0,0 +1,7 @@
+package com.java.gamblingsimulator;
+
+public class Gambling_Simulator {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Game Simulator program");
+	}
+}
